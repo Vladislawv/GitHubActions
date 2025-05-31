8cd3f34dd4dfb44ff3f4be04892037852e15e730
v.yusupov <vladusupov2810@gmail.com>
2025-05-31 20:36:20 +0300

    (feat): Initial commit with test class.

---
+++ GitHubActions/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace GitHubActions.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public void CheckAge()
+    {
+        if (Age < 18) throw new Exception();
+    }
+}
---
+++ GitHubActions/Tests/Unit/UserTests/CheckAgeTests.cs
@@ -0,0 +1,33 @@
+﻿using GitHubActions.Entities;
+using Xunit;
+
+namespace GitHubActions.Tests.Unit.UserTests;
+
+public class CheckAgeTests
+{
+    [Fact]
+    public void CheckAge_LessThanEighteen_ShouldThrowException()
+    {
+        // Arrange
+        var user = new User { Age = 17, Name = "Colin" };
+        
+        // Act
+        var act = () => user.CheckAge();
+        
+        // Assert
+        Assert.Throws<Exception>(act);
+    }
+    
+    [Fact]
+    public void CheckAge_GreaterThanEighteen_ShouldNotThrowException()
+    {
+        // Arrange
+        var user = new User { Age = 18, Name = "Colin" };
+        
+        // Act
+        var act = Record.Exception(() => user.CheckAge());
+
+        // Assert
+        Assert.Null(act);
+    }
+}
